4bc1e39358210e1a9c11a91a9ee359cff15a3377
Ladi Oyeleye <loyeleye@gmail.com>
2018-12-05 11:45:57 -0500

    Add database support

--- evil_third_party.py
+++ evil_third_party.py
@@ -1,12 +1,12 @@
 from flask import Flask, Markup, make_response, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from urllib.parse import parse_qs
+from flask_heroku import Heroku
 import uuid
+import sys
 import datetime
 app = Flask(__name__)
-from flask_heroku import Heroku
 
-app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'CIS 4851'
 heroku = Heroku(app)
@@ -43,6 +43,32 @@ def create_advertisement(title):
             </body>
         </html>
     ''')
+
+
+class UrlTuple(db.Model):
+    __tablename__ = "url_tuples"
+    id = db.Column(db.Integer, primary_key=True)
+    cookie_id = db.Column(db.Text())
+    url = db.Column(db.Text())
+    timestamp = db.Column(db.text())
+
+    def __init__(self, cookie_id, url, timestamp):
+        self.cookie_id = cookie_id
+        self.url = url
+        self.timestamp = timestamp
+
+
+class FingerprintTuple(db.Model):
+    __tablename__ = "fingerprint_tuples"
+    id = db.Column(db.Integer, primary_key=True)
+    cookie_id = db.Column(db.Text())
+    fingerprint_hash = db.Column(db.Text())
+    timestamp = db.Column(db.text())
+
+    def __init__(self, cookie_id, fingerprint_hash, timestamp):
+        self.cookie_id = cookie_id
+        self.fingerprint_hash = fingerprint_hash
+        self.timestamp = timestamp
 
 
 hacker_group_name = 'hackers_group'
@@ -103,28 +129,31 @@ def evil_third_party():
         #fingerprint_tuples = [] # cookie_id fingerprint_hash timestamp
 
         # grab the fingerprints that match the current cookie_id
-        linked_fingerprint_tuples = filter(
-            lambda fingerprint_tuple: fingerprint_tuple[0] == cookie_id,
-            fingerprint_tuples
-        )
-        linked_fingerprints = list(map(
-            lambda fingerprint_tuple: fingerprint_tuple[1],
-            linked_fingerprint_tuples
-        ))
+        matched_fingerprints = FingerprintTuple.query.filter_by(cookie_id = cookie_id).with_entities(FingerprintTuple.fingerprint_hash)
+        # linked_fingerprint_tuples = filter(
+        #     lambda fingerprint_tuple: fingerprint_tuple[0] == cookie_id,
+        #     fingerprint_tuples
+        # )
+        # linked_fingerprints = list(map(
+        #     lambda fingerprint_tuple: fingerprint_tuple[1],
+        #     linked_fingerprint_tuples
+        # ))
 
         # grab the cookies that are linked to the fingerprints found above
-        linked_cookie_tuples = filter(
-            lambda fingerprint_tuple: fingerprint_tuple[1] in linked_fingerprints,
-            fingerprint_tuples
-        )
-        linked_cookies = list(map(
-            lambda fingerprint_tuple: fingerprint_tuple[0],
-            linked_cookie_tuples
-        ))
+        matched_cookies = FingerprintTuple.query.filter(FingerprintTuple.fingerprint_hash.in_(matched_fingerprints)).with_entities(FingerprintTuple.cookie_id)
+        # linked_cookie_tuples = filter(
+        #     lambda fingerprint_tuple: fingerprint_tuple[1] in linked_fingerprints,
+        #     fingerprint_tuples
+        # )
+        # linked_cookies = list(map(
+        #     lambda fingerprint_tuple: fingerprint_tuple[0],
+        #     linked_cookie_tuples
+        # ))
 
         # grab the urls that are linked to the cookies
-        linked_url_tuples = filter(lambda url_tuple: url_tuple[0] in linked_cookies, url_tuples)
-        linked_urls = list(map(lambda url_tuple: url_tuple[1], linked_url_tuples))
+        matched_urls = UrlTuple.query.filter(UrlTuple.cookie_id.in_(matched_cookies)).with_entities(UrlTuple.url)
+        # linked_url_tuples = filter(lambda url_tuple: url_tuple[0] in linked_cookies, url_tuples)
+        # linked_urls = list(map(lambda url_tuple: url_tuple[1], linked_url_tuples))
 
         def parse_url(url):
             split = url.split('?')
@@ -132,7 +161,7 @@ def evil_third_party():
                 return {}
             query_string = split[1]
             return parse_qs(query_string)
-        parsed_urls = list(map(parse_url, linked_urls))
+        parsed_urls = list(map(parse_url, matched_urls))
 
         combined_dict = {}
 
@@ -155,6 +184,7 @@ def evil_third_party():
     # and grab PII leaked from the URL
     url_tuple = (cookie_id, url, timestamp)
     url_tuples.append(url_tuple)
+    write_url_tuple_to_db(cookie_id, url, timestamp)
 
     return response
 
@@ -175,8 +205,35 @@ def fingerprints():
 
     fingerprint_tuple = (cookie_id, fingerprint, timestamp)
     fingerprint_tuples.append(fingerprint_tuple)
+    write_fingerprint_tuple_to_db(cookie_id, fingerprint, timestamp)
     
-    return response
+    return
+
+def write_fingerprint_tuple_to_db(cookie_id, fingerprint, timestamp):
+    db_data = FingerprintTuple(cookie_id, fingerprint, timestamp)
+    log_data = db_data.__dict__.copy()
+    del log_data["_sa_instance_state"]
+    try:
+        db.session.add(db_data)
+        db.session.commit()
+    except Exception as e:
+        print("\n FAILED entry: {}\n".format(jsonify(log_data)))
+        print(e)
+        sys.stdout.flush()
+    return
+
+def write_url_tuple_to_db(cookie_id, url, timestamp):
+    db_data = UrlTuple(cookie_id, url, timestamp)
+    log_data = db_data.__dict__.copy()
+    del log_data["_sa_instance_state"]
+    try:
+        db.session.add(db_data)
+        db.session.commit()
+    except Exception as e:
+        print("\n FAILED entry: {}\n".format(jsonify(log_data)))
+        print(e)
+        sys.stdout.flush()
+    return
 
 @app.route('/reset')
 def reset():
